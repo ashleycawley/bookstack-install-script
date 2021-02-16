3f68e74ea47e8b457329d920ae8901cfad26d0b6
Ashley Cawley <ash@ashleycawley.co.uk>
2021-02-16 17:25:03 +0000

    Proof of concept working so far

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bookstack-install-script
 A unofficial automatic installation script for BookStack - bookstackapp.com
+
+Operating System: CentOS / RedHat systems
+
+Prerequisites:
+This script assumes that you have the following already installed:
+git
+composer
+NGINX
+MySQL 5.7.33 or newer
+PHP 7.2 or PHP 7.3 (php72u php72u-cli php72u-fpm php72u-gd php72u-json php72u-mbstring php72u-mysqlnd php72u-openssl php72u-tidy php72u-tokenizer php72u-xml)
--- bookstack-install-script.sh
+++ bookstack-install-script.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
 
+# Notes:
+
+# Exit Codes:
+# 0 - OK
+
+# Variables
+WEB_ROOT="/home/user/site/public_html" # Full Path. ie. /home/user/public_html
+DB_NAME="oq9J3dbVExcL"
+DB_USER="oq9J3dbVExcL"
+DB_PASS='r&R%.>2^)TVx'
+DB_PASS_SANITISED=$(echo $DB_PASS | sed 's#\([]\#\%\@\*\$\/&[]\)#\\\1#g')
+
+# Main Script
+cd "$WEB_ROOT"
+
+# Pulls down BookStack from Repository
+git clone https://github.com/BookStackApp/BookStack.git --branch release --single-branch
+
+cd ./BookStack/
+
+# Install using Composer
+composer install --no-dev
+
+# Setup configuration file
+cp .env.example .env
+
+# Updates configuration file with our database details
+sed -i "s/database_database/$DB_NAME/g" .env
+sed -i "s/database_username/$DB_USER/g" .env
+sed -i "s/database_user_password/$DB_PASS_SANITISED/g" .env
+
+# Generates Unique Application Key
+php artisan key:generate --force
+
+# Populates database with data structure
+php artisan migrate --force
