a79101e97c71b67c6adb5e3d54db4f7de2407bcc
Ashley Cawley <ash@ashleycawley.co.uk>
2021-05-25 20:34:55 +0100

    Building more menus

--- bookstack-install-script.sh
+++ bookstack-install-script.sh
@@ -12,51 +12,106 @@
 
 # Exit Codes:
 # 0 - OK
+# 1 - The user ran the script as root instead of a normal user
+# 2 - User exited whiptail menu whilst at data gathering stage
+
+# Variables
+WEB_ROOT="/home/user/$(whoami)/public_html" # Full Path. ie. /home/user/public_html
+# $(echo "$(hostname)$(hostname -I)$(date +%s)$(echo $RANDOM)" | sha1sum | cut -c1-12) # Randomly generated database password
+
+# Root Check
+if [ `whoami` == "root" ]
+then
+    whiptail --title "! ~ Root User Detected ~ !" --msgbox "You ran this script as root, the police have been informed..." 8 65
+    whiptail --title "! ~ Root User Detected ~ !" --msgbox "Please run this script as the standard web user, not root." 8 65
+    echo "Go find the Scout Web User <3"
+    exit 1
+else
+    USER=$(whoami)
+	fi
 
 # Whiptail GUI Menu gathering settings from the sysadmin
-
-DB_URL$(whiptail --inputbox "Ener in the Site URL (with NO trailing slash):" 8 39 https:// --title "Site URL" 3>&1 1>&2 2>&3)
+SITE_URL=$(whiptail --inputbox "Ener in the Site URL (with NO trailing slash):" 8 65 https:// --title "Site URL" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
     echo ""
 else
     echo "Cancelled."
+    exit 2
 fi
 
-DB_NAME=$(whiptail --inputbox "Enter in the Database Name that Scout provided to you:" 8 39 --title "Database Name" 3>&1 1>&2 2>&3)
+DB_NAME=$(whiptail --inputbox "Enter in the Database Name that Scout provided to you:" 8 65 --title "Database Name" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
     echo ""
 else
     echo "Cancelled."
+    exit 2
 fi
 
 DB_USER=$(echo $DB_NAME) # In Scout the Database Name and Username are the same, so we are deriving the database username automagically.
 
-DB_PASS=$(whiptail --inputbox "Enter in the Database Password that Scout provided to you:" 8 39 --title "Database Password" 3>&1 1>&2 2>&3)
+DB_PASS=$(whiptail --inputbox "Enter in the Database Password that Scout provided to you:" 8 65 --title "Database Password" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
     echo ""
 else
     echo "Cancelled."
+    exit 2
 fi
-
-
-
-# Variables
-WEB_ROOT="/home/user/site/public_html" # Full Path. ie. /home/user/public_html
-DB_NAME="oq9J3dbVExcL"
-
-# $(echo "$(hostname)$(hostname -I)$(date +%s)$(echo $RANDOM)" | sha1sum | cut -c1-12) # Randomly generated database password
 
 # Sanitising user supplied variables to work nicely with sed later on
 DB_PASS_SANITISED=$(echo $DB_PASS | sed 's#\([]\#\%\@\*\$\/&[]\)#\\\1#g')
 SITE_URL_SANITISED=$(echo $SITE_URL | sed 's#\([]\#\%\@\*\$\/&[]\)#\\\1#g')
 
+
+echo " ### Settings Review ###
+
+ User:       $USER
+ Web Root:   /home/$USER/site/public_html
+ Website:    $SITE_URL
+ DB Name:    $DB_NAME
+ DB Pass:    $DB_PASS_SANITISED
+
+
+" && sleep 5
+
 # Main Script
-cd "$WEB_ROOT"
+cd "$WEB_ROOT" &>/dev/null
+WEB_ROOT_DIR_STATUS=$(echo $?)
+
+if [ "$WEB_ROOT_DIR_STATUS" != "0" ]
+then
+    whiptail --title "Web Root Problem" --msgbox "I do beg your pardon, but I thought the user was $USER and on a Scout server their web root should be at: $WEB_ROOT but I can't see that directory on disk." 10 65
+
+    if (whiptail --title "New Directory?" --yesno "Would you like to supply a new Web Root path?" 8 65); then
+    
+    WEB_ROOT=$(whiptail --inputbox "Enter a new Web Root path:" 8 65 --title "New Web Root" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        cd "$WEB_ROOT" &>/dev/null
+        WEB_ROOT_DIR_STATUS=$(echo $?)
+        if [ "$WEB_ROOT_DIR_STATUS" != "0" ]
+        then
+            echo "There is still a problem with $WEB_ROOT so we're now exiting..."
+            exit 2
+        else
+            echo "Great, $WEB_ROOT works for me, so lets install BookStack there then..." && sleep 5
+        fi
+    else
+        echo "Cancelled."
+        exit 2
+    fi
+
+    else
+    echo "You selected No, I guess we'll try again another day?"
+    exit 2
+    fi
+
+fi
 
 # Pulls down BookStack from Repository
+echo "Pausing before cloning...." && sleep 60
 git clone https://github.com/BookStackApp/BookStack.git --branch release --single-branch
 
 cd ./BookStack/
